fc69bb2e02e0fbd1047e703fd9a32e04d7b09400
azkazunami36 <kazunami36.sum@icloud.com>
2024-02-04 01:45:43 +0900

    to easyExpress Update

--- main.ts
+++ main.ts
@@ -10,18 +10,12 @@ const ytdl = SumLibrary.externalModules.ytdl;
 // 完成を高速でできるよう適当に作成したため、拡張性がない恐れがありますが、ご了承ください。
 // if文などが多く占めている可能性があります。
 
+const easyapp = new SumLibrary.orignalModules.easyExpress()
+
 const expressApp = express();
 
 const server = https.createServer(expressApp);
 
-expressApp.use(bodyParser.urlencoded({ limit: "127gb", extended: true }));
+const basePath = "/dataFolder";
 
-const basePath = "/dataFolder"
-
-if (fs.existsSync(basePath)) fs.rmdirSync(basePath)
-
-expressApp.get("*", (req, res) => {
-    const URL = req.url;
-    
-
-});
+if (fs.existsSync(basePath)) fs.rmdirSync(basePath);
