63d8e0b1bd86a16814f8de899b4fd28a70e058e7
azkazunami36 <kazunami36.sum@icloud.com>
2024-02-11 02:55:22 +0900

    コードの小修整

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import https from "https";
 import fs from "fs";
 
 import SumLibrary from "./variousLibrary/sumLibrary.js";
-import bodyParser from "body-parser";
+
 const ytdl = SumLibrary.externalModules.ytdl;
 
 // このプログラムは綺麗に作られていません。
@@ -11,10 +11,6 @@ const ytdl = SumLibrary.externalModules.ytdl;
 // if文などが多く占めている可能性があります。
 
 const easyapp = new SumLibrary.orignalModules.easyExpress()
-
-const expressApp = express();
-
-const server = https.createServer(expressApp);
 
 const basePath = "/dataFolder";
 
